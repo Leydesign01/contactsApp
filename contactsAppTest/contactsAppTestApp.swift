bda86fb71cebe09bd2afc3763403ae381431d61e
leydesign01 <ingmlorenzo2011@gmail.com>
2025-04-30 15:21:18 -0400

    Initial Commit

---
+++ contactsAppTest/contactsAppTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  contactsAppTestApp.swift
+//  contactsAppTest
+//
+//  Created by LorenzoAC on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct contactsAppTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
